7d12bb147246cea1a363f129af9de72fafaeb074
chris <chris@joycenet.us>
2019-08-25 12:41:52 -0500

    working accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule,
   MatButtonModule,
   MatIconModule,
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatExpansionModule,
     MatButtonModule,
     MatIconModule,
